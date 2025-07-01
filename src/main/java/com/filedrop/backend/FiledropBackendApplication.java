73413d3189ac4ca0cf685f44926bf703b62b0d23
eminsayan <emnsyn441@gmail.com>
2025-07-02 00:44:01 +0300

    initial commit

---
+++ src/main/java/com/filedrop/backend/FiledropBackendApplication.java
@@ -0,0 +1,13 @@
+package com.filedrop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledropBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledropBackendApplication.class, args);
+	}
+
+}
