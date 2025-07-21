256cc93d4c797b3fd2cba7b58f0ca90ad5dd67bf
eminsayan <emnsyn441@gmail.com>
2025-07-21 16:13:21 +0300

    setters added to file resource file

--- src/main/java/com/filedrop/backend/controller/resource/FileResource.java
+++ src/main/java/com/filedrop/backend/controller/resource/FileResource.java
@@ -2,13 +2,14 @@ package com.filedrop.backend.controller.resource;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-// Todo (MK): Setter gerekmiyor mu burada? Çalışıyor mu bu?
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class FileResource {
     private UUID id;
